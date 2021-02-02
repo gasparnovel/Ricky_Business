6562106f6cfd3b24ab1e6e3a8e5be7c9b4db3435
gasparnovel <gnovel@cifpfbmoll.eu>
2021-02-02 10:07:14 +0100

    feat: :sparkles: Funcionalidades añadidas en RicksyBusiness.java

--- src/main/java/edu/ricky/business/ricky/RicksyBusiness.java
+++ src/main/java/edu/ricky/business/ricky/RicksyBusiness.java
@@ -23,67 +23,98 @@
  * en la tarjeta del invitado/a el coste de ambos servicios. 
  */
 
-
 package edu.ricky.business.ricky;
 
 import edu.ricky.business.ricky.CreditCard;
 
-
 public class RicksyBusiness {
 
-       public static void main( String[] args ) {
+       public static void main(String[] args) {
               System.out.println();
 
               /**
-               * Crea una tarjeta de crédito para Abradolph.
-               * Como es una AndromedanExpress
-               * el crédito inicial es de 3000 EZIS
+               * Crea una tarjeta de crédito para Abradolph. Como es una AndromedanExpress el
+               * crédito inicial es de 3000 EZIS
                */
 
               CreditCard abradolph = new CreditCard("Abradolph Lincler", "4916119711304546");
 
-              System.out.println("\n" + "Tarjeta de Abradolph" + "\n" + 
-                                          "===================="        );
+              System.out.println("\n" + "Tarjeta de Abradolph" + "\n" + "====================");
               System.out.println(abradolph);
-       
+
               /**
-               * Construye el componente de reserva de Ovnis.
-               * Recibe el objeto tarjeta de crédito del invitado/a
-               * en el método dispatch(card)
-               * y realiza un cargo a la tarjeta.
-               * Si hay saldo suficiente se reserva un UberOvni
-               * de los que estén libres.
-               * El coste del ovni es de 500 EZIs.
+               * Construye el componente de reserva de Ovnis. Recibe el objeto tarjeta de
+               * crédito del invitado/a en el método dispatch(card) y realiza un cargo a la
+               * tarjeta. Si hay saldo suficiente se reserva un UberOvni de los que estén
+               * libres. El coste del ovni es de 500 EZIs.
                */
               UfosPark ufospark = new UfosPark();
 
-              String[] ufosID = {"unx", "dox"};
-                     for (String ovni : ufosID) {
-                            ufospark.add(ovni);
+              String[] ufosID = { "unx", "dox" };
+              for (String ovni : ufosID) {
+                     ufospark.add(ovni);
               }
-       
+
               // Procesamos el pago y reserva de ovni de Abradolph
 
               ufospark.dispatch(abradolph);
 
               // Mostramos el ID del ovni asignado a Abradolph
 
-              System.out.println("\nOvni de Abradolph\n" +
-                                   "==================");
+              System.out.println("\nOvni de Abradolph\n" + "==================");
               System.out.println(ufospark.getUfoOf(abradolph.getNumber()));
-       
+
               // Mostramos el credito de la tarjeta de Abradolph
 
               System.out.println("Credito de Abradolph: " + abradolph.getCredit());
 
               // La dualidad en Abradolph quiere reservar otro ovni.
-              // El sistema detecta que ya tiene uno 
+              // El sistema detecta que ya tiene uno
               // e ignora la petición.
 
-              System.out.println("\nAbradolph quiere otro ovni\n" + 
-                            "==========================");
+              System.out.println("\nAbradolph quiere otro ovni\n" + "==========================");
               ufospark.dispatch(abradolph);
               System.out.println("Su credito no ha cambiado: " + abradolph.getCredit());
               System.out.println("Ovni de Abradolph: " + ufospark.getUfoOf(abradolph.getNumber()));
+
+              // A GearHead le vacía la tarjeta el alien "Cámara Lenta"
+              // mientras le daba la chapa, justo antes de pagar el ovni.
+              // Intenta reservarlo y el componente de reserva de ovnis
+              // no le asigna ninguno.
+
+              System.out.println("\nLLega GearHead!\n" + "===============");
+              CreditCard gearHead = new CreditCard("Gearhead", "8888888888888888");
+
+              gearHead.pay(3000); // le vacían la cartera
+
+              ufospark.dispatch(gearHead);
+              System.out.println("Su credito es cero: " + gearHead.credit());
+              System.out.println("No puede reservar ovni: " + ufospark.getUfoOf(gearHead.number()));
+
+              // Squanchy deja su ovni reservado
+              // antes de irse a squanchear
+
+              System.out.println("\nLLega Squanchy!\n" + "==============");
+              CreditCard squanchy = new CreditCard("Squanchy", "4444444444444444");
+              ufospark.dispatch(squanchy);
+              System.out.println("Su credito es: " + squanchy.credit());
+              System.out.println("Su ovni es: " + ufospark.getUfoOf(squanchy.number()));
+
+              // Morty quiere un ovni para huir de la fiesta
+              // pero ya no queda ninguno disponible
+
+              System.out.println("\nAlgun ovni para Morty?\n" + "======================");
+              CreditCard morty = new CreditCard("Morty", "0000000000000000");
+              ufospark.dispatch(morty);
+              System.out.println("Su credito no ha cambiado: " + morty.credit());
+              System.out.println("No hay ovni Morty: " + ufospark.getUfoOf(morty.number()));
+
+              // Metemos un ovni más en la flota de ovnis
+              // y mostramos la flota por consola
+
+              System.out.println("\nFlota de ovnis\n" + "==============");
+              ufospark.add("trex");
+              System.out.println(ufospark);
+
        }
 }
